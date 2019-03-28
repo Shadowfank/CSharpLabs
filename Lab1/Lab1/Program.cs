03f34e701fe52fd4ca8e247721aaf0881c4f5e57
Shadowfank <34539657+Shadowfank@users.noreply.github.com>
2019-03-28 19:41:03 +0200

    Lab1 Done

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab1
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Введіть перше число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть друге число");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("Результат додавання: {0}", c);
+            Console.ReadKey();
         }
     }
 }
