74bf3f7bf87d236f5cf282c753297a8c2b840606
Shadowfank <34539657+Shadowfank@users.noreply.github.com>
2019-03-28 19:59:37 +0200

    Done

---
+++ Lab4_2/Lab4_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            double a;
+            double h;
+            Console.Write("Введіть одну з сторін трикутника та висоту, опущену на неї \r\nСторона трикутника = ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Висота трикутника = ");
+            h = Convert.ToDouble(Console.ReadLine());
+            double S = 0.5 * a * h;
+            Console.WriteLine("Площа трикутника = " + S);
+            Console.ReadKey();
+        }
+    }
+}
