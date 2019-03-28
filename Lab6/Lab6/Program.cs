9c8ed6e001e6e35164be0d793cce50641d333b25
Shadowfank <34539657+Shadowfank@users.noreply.github.com>
2019-03-28 20:04:14 +0200

    Done

---
+++ Lab6/Lab6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int n; double a = 0;
+            Console.Write("Введіть n=");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                a = 3 - 3 * Math.Sin(Math.Pow(i, 2));
+                Console.WriteLine("a={0}", a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
