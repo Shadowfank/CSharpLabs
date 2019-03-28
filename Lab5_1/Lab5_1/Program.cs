977f2d1a7474902fac2b30c6fd71f5ea63ad6058
Shadowfank <34539657+Shadowfank@users.noreply.github.com>
2019-03-28 20:01:30 +0200

    Done

---
+++ Lab5_1/Lab5_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            double a, b, c;
+            Console.WriteLine("Введіть значення змінної a");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть значення змінної b");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть значення змінної c");
+            c = double.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                double sum = Math.Pow((a + b + c), 2);
+                Console.WriteLine("a > b, тому значення додавання =" + sum);
+                Console.ReadLine();
+            }
+            else
+            {
+                double dobutok = Math.Pow((a * b * c), 2);
+                Console.WriteLine("a < b, тому значення добутку =" + dobutok);
+                Console.ReadLine();
+            }
+        }
+    }
+}
