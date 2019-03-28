e4044de01d9ae97266997434e7819b348fce0b17
Shadowfank <34539657+Shadowfank@users.noreply.github.com>
2019-03-28 21:48:21 +0200

    need rework

--- Lab7_1/Lab7_1/Program.cs
+++ Lab7_1/Lab7_1/Program.cs
@@ -10,6 +10,14 @@ namespace Lab7_1
     {
         static void Main(string[] args)
         {
+            double i = -3;
+            while (i >= -3 && i <= 2)
+            {
+                double y = 3-3*Math.Sin(Math.Pow(i, 2));
+                Console.WriteLine("Значення виразу при i " + i + " буде " + y);
+                i = i + 0.1;
+            }
+            Console.ReadKey();
         }
     }
 }
